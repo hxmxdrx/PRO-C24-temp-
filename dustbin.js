183d78ba6e06d78aae2a715badabfb56e4a920fa
hxmxdrx <75106013+hxmxdrx@users.noreply.github.com>
2020-11-28 19:25:43 -0500

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,23 @@
+var width
+var boxPosition, boxleftSprite
+var boxLeftBody, boxRightBody
+boxPosition = width/2-100
+	boxY=610;
+
+	boxleftSprite = createSprite(boxPosition, boxY, 20,100);
+	boxleftSprite.shapeColor= color(255,0,0);
+
+	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+	World.add(world, boxLeftBody);
+
+	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+	boxBase.shapeColor=color(255,0,0);
+
+	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+	World.add(world, boxBottomBody);
+
+	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
+	boxleftSprite.shapeColor=color(255,0,0);
+
+	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+	World.add(world, boxRightBody);
